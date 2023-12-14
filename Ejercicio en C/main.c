3909581a1f28b6b53ec84fba6ed9f88e3bcb7621
rodrigoyr <ryeperub@myuax.com>
2023-12-14 12:29:20 +0100

    Estructura del Estudiante c code commit

--- Ejercicio en C/main.c
+++ Ejercicio en C/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+};
+
+void mostrarEstudiante(struct Estudiante estudiante);
+
 int main() {
-    printf("Hello, World!\n");
+    struct Estudiante estudiante1 = {"Rodrigo", 18, 9.5};
+
+    mostrarEstudiante(estudiante1);
+
     return 0;
+}
+
+void mostrarEstudiante(struct Estudiante estudiante) {
+    printf("Nombre: %s\n", estudiante.nombre);
+    printf("Edad: %d\n", estudiante.edad);
+    printf("Promedio: %.2f\n", estudiante.promedio);
 }
