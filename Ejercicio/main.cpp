230ac5dd5d7b0d335a2bef521ef0364f1916a919
rodrigoyr <ryeperub@myuax.com>
2023-12-14 12:06:51 +0100

    error code commit

--- Ejercicio/main.cpp
+++ Ejercicio/main.cpp
@@ -55,51 +55,23 @@ void agregarMateria(Estudiante& estudiante, const string& materia) {
 }
 
 void eliminarMateria(Estudiante& estudiante, const string& materia) {
-    estudiante.materias.erase(estudiante.materias.begin());
+    auto it = find(estudiante.materias.begin(), estudiante.materias.end(), materia);
+    if (it != estudiante.materias.end()) {
+        estudiante.materias.erase(it);
+    } else {
+        throw MateriaNoEncontradaException();
+    }
 }
 
 void registrarAsistencia(Estudiante& estudiante, const string& fecha, const string& materia, const string& estado) {
     auto duplicado = find_if(estudiante.asistencias.begin(), estudiante.asistencias.end(),
                              [&fecha, &materia](const Asistencia& a) { return a.fecha == fecha && a.materia == materia; });
 
-    Asistencia asistencia;
-    asistencia.fecha = fecha;
-    asistencia.materia = materia;
-    asistencia.estado = estado;
-    estudiante.asistencias.push_back(asistencia);
-}
-
-int main() {
-    Estudiante estudiante1;
-
-    try {
-        estudiante1.nombre = "Juan Perez";
-        estudiante1.edad = 20;
-        estudiante1.promedio = 8.5;
-
-        agregarMateria(estudiante1, "Latin");
-        agregarMateria(estudiante1, "Programacion");
-        agregarMateria(estudiante1, "Historia");
-
-        mostrarEstudiante(estudiante1);
-
-        try {
-            eliminarMateria(estudiante1, "Biologia");
-        } catch (const MateriaNoEncontradaException& e) {
-            cerr << "Error: " << e.what() << endl;
-        }
-
-        try {
-            registrarAsistencia(estudiante1, "2023-12-17", "Historia", "asistio");
-        } catch (const AsistenciaDuplicadaException& e) {
-            cerr << "Error: " << e.what() << endl;
-        }
-
-        mostrarEstudiante(estudiante1);
-
-    } catch (const exception& e) {
-        cerr << "Error general: " << e.what() << endl;
-    }
-
-    return 0;
-}
+    if (duplicado == estudiante.asistencias.end()) {
+        Asistencia asistencia;
+        asistencia.fecha = fecha;
+        asistencia.materia = materia;
+        asistencia.estado = estado;
+        estudiante.asistencias.push_back(asistencia);
+    } else {
+        throw
