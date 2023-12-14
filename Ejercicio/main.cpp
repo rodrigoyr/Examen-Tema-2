b84f0719401e44e49561b6a235938a4c8b6b4444
rodrigoyr <ryeperub@myuax.com>
2023-12-14 11:55:20 +0100

    error code commit

--- Ejercicio/main.cpp
+++ Ejercicio/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -7,12 +8,26 @@ struct Estudiante {
     string nombre;
     int edad;
     float promedio;
+    vector<string> materias; // Lista de materias
 };
 
 void mostrarEstudiante(const Estudiante& estudiante) {
     cout << "Nombre: " << estudiante.nombre << endl;
     cout << "Edad: " << estudiante.edad << endl;
     cout << "Promedio: " << estudiante.promedio << endl;
+
+    cout << "Materias inscritas:" << endl;
+    for (const auto& materia : estudiante.materias) {
+        cout << "- " << materia << endl;
+    }
+}
+
+void agregarMateria(Estudiante& estudiante, const string& materia) {
+    estudiante.materias.push_back(materia);
+}
+
+void eliminarMateria(Estudiante& estudiante, const string& materia) {
+    estudiante.materias.erase(remove(estudiante.materias.begin(), estudiante.materias.end(), materia), estudiante.materias.end());
 }
 
 int main() {
@@ -21,6 +36,14 @@ int main() {
     estudiante1.nombre = "Juan Perez";
     estudiante1.edad = 20;
     estudiante1.promedio = 8.5;
+
+    agregarMateria(estudiante1, "Matemáticas");
+    agregarMateria(estudiante1, "Programación");
+    agregarMateria(estudiante1, "Historia");
+
+    mostrarEstudiante(estudiante1);
+
+    eliminarMateria(estudiante1, "Historia");
 
     mostrarEstudiante(estudiante1);
 
